cb903c715eb686593bc50405267622ede0cfe8e5
Ben Hermann <ben.hermann@googlemail.com>
2017-12-21 15:08:42 +0100

    Changed annotations

--- src/main/java/de/upb/cs/swt/sectypes/qual/Low.java
+++ src/main/java/de/upb/cs/swt/sectypes/qual/Low.java
@@ -1,7 +1,6 @@
 package de.upb.cs.swt.sectypes.qual;
 
-import org.checkerframework.framework.qual.SubtypeOf;
-import org.checkerframework.framework.qual.Unqualified;
+import org.checkerframework.framework.qual.*;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
@@ -9,6 +8,8 @@ import java.lang.annotation.Target;
 /**
  * The low-security qualifier
  */
-@SubtypeOf(Unqualified.class)
+@SubtypeOf(High.class)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@ImplicitFor(literals = LiteralKind.ALL)
+@DefaultQualifierInHierarchy
 public @interface Low {}
